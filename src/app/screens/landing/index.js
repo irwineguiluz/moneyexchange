186a78228a924ab65f2391b9172b07e5cf2e9e3f
Irwin Eguiluz <irwin.eguiluz@glr.pe>
2020-02-11 14:03:41 -0500

    to install mask input

--- src/app/screens/landing/index.js
+++ src/app/screens/landing/index.js
@@ -1,32 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
+import CurrencyFormat from 'react-currency-format';
 
-const Landing = () => (
-    <div>
-        <Header />
-        <section className="currencies-converter__container">
+class Landing extends Component {
+    state = {
+      dollarsInput: '',
+      eurosInput: '',
+    }
+
+    resetInputs = () => {
+      this.setState({
+        dollarsInput: '',
+        eurosInput: '',
+      })
+    }
+
+    calculateCurrency = () => {
+      if (this.state.dollarsInput > 0) {
+        this.setState({
+          eurosInput: this.state.dollarsInput,
+        });
+      } else {
+        this.resetInputs();
+      }
+    }
+
+    handleInput = (e) => {
+      this.setState({
+        dollarsInput: e.value,
+      });
+    }
+
+    render () {
+      const {
+        dollarsInput,
+        eurosInput,
+      } = this.state;
+
+      return (
+        <div>
+          <Header />
+          <section className="currencies-converter__container">
             <div className="currencies-converter__wrapper">
-                <div className="currencies-converter">
-                    <div className="currencies-inputs__wrapper">
-                        <h1 className="title">Find out how much the euro is today!</h1>
-                        <div className="currencies-inputs">
-                            <div className="currency-input__wrapper">
-                                <input type="text" className="currency-input" placeholder="USD ($)" />
-                            </div>
-                            <div className="currency-input__wrapper">
-                                <input type="text" className="currency-input" placeholder="Euros (€)" readOnly />
-                            </div>
-                        </div>
-                        <div className="currencies-converter__button">
-                            <button>Calculate!</button>
-                        </div>
+              <div className="currencies-converter">
+                <div className="currencies-inputs__wrapper">
+                  <h1 className="title">Find out how much the euro is today!</h1>
+                  <div className="currencies-inputs">
+                    <div className="currency-input__wrapper">
+                      <CurrencyFormat
+                        className="currency-input"
+                        placeholder="USD ($) Ex. $ 000,000.0000"
+                        thousandSeparator={true}
+                        prefix={'$ '}
+                        decimalScale={4}
+                        value={dollarsInput}
+                        onValueChange={this.handleInput}
+                      />
                     </div>
+                    <div className="currency-input__wrapper">
+                      <CurrencyFormat
+                        className="currency-input readonly"
+                        placeholder="Euros (€)"
+                        thousandSeparator={true}
+                        prefix={'€ '}
+                        decimalScale={4}
+                        value={eurosInput}
+                        readOnly
+                      />
+                    </div>
+                  </div>
+                  <div className="currencies-converter__button">
+                    <button onClick={this.calculateCurrency}>Calculate!</button>
+                  </div>
                 </div>
+              </div>
             </div>
-        </section>
-        <Footer />
-    </div>
-)
+          </section>
+          <Footer />
+        </div>
+      );
+    }
+}
 
 export default Landing;
